7d209d0951baa3a07facc950f4278798769d1edb
RaiAbhishek <126377241+allakri@users.noreply.github.com>
2025-01-16 17:03:24 +0530

    pj-6 channels/websocket

--- project 6/mysite/chatapp/consumers.py
+++ project 6/mysite/chatapp/consumers.py
@@ -1,24 +1,74 @@
 from channels.generic.websocket import AsyncWebsocketConsumer
 import json
 from asgiref.sync import sync_to_async
+from django.contrib.auth.models import User
+from .models import ChatRoom, ChatMessage
 
 class ChatConsumer(AsyncWebsocketConsumer):
-    
-    
     async def connect(self):
+        # Retrieve the room name from the URL route
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = 'chat_%s' % self.room_name
         
+        # Add the user to the corresponding group
         await self.channel_layer.group_add(
             self.room_group_name,
             self.channel_name
         )
         
-        
+        # Accept the WebSocket connection
         await self.accept()
         
-    async def disconnect(self):
+    async def disconnect(self, close_code):
+        # Remove the user from the group when the WebSocket closes
         await self.channel_layer.group_discard(
-            self.channel_layer,
-            self.room_group_name
+            self.room_group_name,  # Use the room group name, not self.channel_layer
+            self.channel_name
         )
+        
+    async def receive(self, text_data):
+        # Parse the incoming message data
+        data = json.loads(text_data)
+        message = data['message']
+        username = data['username']
+        room = data['room']
+        
+        # Broadcast the message to the group
+        await self.channel_layer.group_send(
+            self.room_group_name, {
+                'type': 'chat_message',
+                'message': message,
+                'username': username,
+                'room': room,
+            }
+        )
+        
+        # Save the message to the database asynchronously
+        await self.save_message(username, room, message)
+    
+    async def chat_message(self, event):
+        # When a message is received from the group, send it to WebSocket clients
+        message = event['message']
+        username = event['username']
+        room = event['room']
+        
+        # Send the message data back to the client (WebSocket)
+        await self.send(text_data=json.dumps({
+            'message': message,
+            'username': username,
+            'room': room,
+        }))
+    
+    @sync_to_async
+    def save_message(self, username, room, message):
+        try:
+            # Fetch the user and room from the database
+            user = User.objects.get(username=username)
+            room = ChatRoom.objects.get(slug=room)
+            
+            # Create and save the new message
+            ChatMessage.objects.create(user=user, room=room, message_content=message)
+        except User.DoesNotExist:
+            pass  # Handle user not found scenario
+        except ChatRoom.DoesNotExist:
+            pass  # Handle room not found scenario
--- project 6/mysite/chatapp/routing.py
+++ project 6/mysite/chatapp/routing.py
@@ -1,6 +1,12 @@
-from django.urls import path
-from . import consumers
+# Import the required modules
+from django.urls import path  # Import path function to define URL patterns
+from . import consumers  # Import the consumers module where WebSocket consumers are defined
 
+# Define the WebSocket URL patterns
 websocket_urlpatterns = [
-    path('ws/<str:room_name>',consumers.ChatConsumer.as_asgi()),
+    # Define a WebSocket route (URL pattern) that takes a room_name as a string parameter
+    # The URL will match any URL like /ws/<room_name>/, where <room_name> is the name of the chat room
+    path('ws/<str:room_name>/', consumers.ChatConsumer.as_asgi()),
+    # The above URL pattern will be matched when the WebSocket client connects with a URL like:
+    # ws/example_room/ or ws/room_name/
 ]
